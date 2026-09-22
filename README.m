7fc288ee4230ae6ff071df1bd17fe6e2ea519cf1
Farah Abuassi <farahabuassi16@gmail.com>
2026-09-22 13:38:01 +0300

    Create README.md for EFCore Data Management
    Add README for EFCore Data Management project with features.

---
+++ README.m
@@ -0,0 +1,11 @@
+# EFCore Data Management
+
+A simple .NET Console Application using Entity Framework Core and SQLite.
+
+### Features
+
+* Categories and Products
+* One-to-Many Relationship
+* CRUD Operations
+* Eager Loading with `Include()`
+* JSON Serialization
